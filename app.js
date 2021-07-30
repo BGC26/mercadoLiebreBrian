2b637d25f8a01beb3b6eb57b11a673f3c039df05
BGC26 <briancatrini87@gmail.com>
2021-07-30 19:25:26 -0300

    nuevocambio

--- app.js
+++ app.js
@@ -9,10 +9,6 @@ app.listen(process.env.PORT || 3030, function() {
 
 app.use (express.static(publicPath));
 
-app.get("/", function (req, res) {
-    res.send("Hola mundo!");
-});
-
 app.get("/home", function(req, res){
     res.sendFile (path.join(__dirname, "/views/index.html"));
 });
@@ -23,6 +19,10 @@ app.get("/register", function(req, res) {
 
 app.get("/login", function(req, res) {
     res.sendFile (path.join(__dirname, "/views/login.html"));
+});
+
+app.get("/", function (req, res) {
+    res.send("Hola mundo!");
 });
 
 app.get("*", function (req, res){
