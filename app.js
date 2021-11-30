1b7725919d305b4db58e5acb518edfce8c2ac3ff
BGC26 <briancatrini87@gmail.com>
2021-11-30 01:37:22 -0300

    corrigiendo mercado liebre

---
+++ app.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const publicPath = path.resolve (__dirname, "./public");
+
+app.listen(process.env.PORT || 3030, function() {
+    console.log("La app esta funcionando en http://localhost:3030");
+});
+
+app.use (express.static(publicPath));
+
+app.get("/", function (req, res) {
+    res.send("Hola mundo!");
+});
+
+app.get("/home", function(req, res){
+    res.sendFile (path.join(__dirname, "/views/index.html"));
+});
+
+app.get("/register", function(req, res) {
+    res.sendFile (path.join(__dirname, "/views/register.html"));
+});
+
+app.get("/login", function(req, res) {
+    res.sendFile (path.join(__dirname, "/views/login.html"));
+});
+
+app.get("*", function (req, res){
+    res.send("NOT FOUND", 404);
+});
+
+
